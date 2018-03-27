58149fe2a26178403053ddbae1fc069632ef806d
jackvnimble <jack@mavenlink.com>
2018-03-27 10:45:58 -0700

    Raise "Unsupported" when attempting to use
    This library is no longer supported. See develeper.mavenlink.com for API
    documentation.

--- lib/mavenlink.rb
+++ lib/mavenlink.rb
@@ -1,3 +1,4 @@
+raise "Unsupported"
 require File.expand_path(File.join(File.dirname(__FILE__), 'mavenlink', 'version'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'mavenlink', 'base'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'mavenlink', 'client'))
